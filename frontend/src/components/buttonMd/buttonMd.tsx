3ce0e3dd80a34fa4cee86e5897b7e70d3a62fc77
karri <nekomarait@gmail.com>
2026-03-27 12:41:34 +0500

    компоненты батонМд и батонСд

---
+++ frontend/src/components/buttonMd/buttonMd.tsx
@@ -0,0 +1,15 @@
+import styles from "./buttonMd.module.css";
+
+interface ButtonProps {
+  title: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+}
+
+export default function ButtonM({ title, type = "button", onClick }: ButtonProps) {
+  return (
+    <button className={styles.button} type={type} onClick={onClick}>
+      {title}
+    </button>
+  );
+}
